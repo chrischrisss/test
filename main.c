e60aed489a44bf3180318972159ec16bd78e2427
Christian <christian@ucmerced-10-34-98-190.ucmerced.edu>
2026-01-21 10:34:16 -0800

    Pointers and arrays 5.3

--- main.c
+++ main.c
@@ -4,18 +4,50 @@ int main() {
     
     int x = 1;
     int y = 2;
-    int z[10];
+    int z[5] = {10, 20, 30, 40, 50};
+
+
 
     int *ip;
     ip = &x; //The pointer changes
     y = *ip;
-    printf("%p\n", (void *)ip);
+    // printf("%p\n", (void *)ip);
     *ip = 0; //The value pointed to changes
-    ip = &z[0];
+    
+    /*
+    These two are the same
+    */
+    ip = &z[0]; 
+    ip = z;
 
-    printf("%p\n", (void *)&x);
-    printf("%d\n", (int)x);
+    ip = &z[1];
+    // printf("%p\n", (void *)ip);
+   
+    //gets the same resutls
+    int result1 = z[1];
+    int restult2 = * (z + 1); //you are derefrencing the 2nd element in the array
+    // printf("%d\n", result1);
+    // printf("%d\n",restult2);
+
+    /*
+    
+    Adding a & to both sides:
+    & (z[1])
+    & (* (z + 1))
+    Therfore, the first one give the address to the 2nd element
+    The seconds one simplifies to z + 1, which also gives the address of the 2nd element
+
+    */
+
+    ip = &z[1];
     printf("%p\n", (void *)ip);
+    ip = z + 1;
+    printf("%p\n", (void *)ip);
+
+
+    // printf("%p\n", (void *)&x);
+    // printf("%d\n", (int)x);
+    // printf("%p\n", (void *)ip);
 
     
 
